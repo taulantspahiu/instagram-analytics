bacf26b431e12786c7284caa25a13b68f2e5fa1c
Taulant Spahiu <spahiut@gmail.com>
2018-02-24 17:13:15 +0100

    fix:fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     instagram: {
-        clientID: '1dd735ec892f47399100dacef6b585a1',
-        clientSecret: 'f108a7f503aa43dd85517ae1bc7095bf',
-        callbackURL: 'http://localhost:3000/auth/instagram/callback'
+        clientID: 'xxx',
+        clientSecret: 'xxx',
+        callbackURL: 'http://xxx:yyy/auth/instagram/callback'
     }
 }
